349887f06ead4652092f480f695f6e66a0c13182
Dario Benevento <dario.benevento@georgfischer.com>
2023-01-08 19:51:03 +0100

    First commit

--- BlazorAlarmClock/Client/Components/Alarm.razor.cs
+++ BlazorAlarmClock/Client/Components/Alarm.razor.cs
@@ -12,15 +12,16 @@ public partial class Alarm
         NONE = 0x00,
         PLAYING = 0x01,
         SNOOZED = 0x02,
-        STOPPED = 0x03
+        STOPPED = 0x03,
+        STOPPED_TODAY = 0x04
 
     }
 
 
 
     [Parameter][EditorRequired] public string FileName { get; set; } = "audio/file1.mp3";
-    [Parameter][EditorRequired] public int AlarmHour { get; set; }
-    [Parameter][EditorRequired] public int AlarmMinute { get; set; }
+    [Parameter]public int AlarmHour { get; set; }
+    [Parameter]public int AlarmMinute { get; set; }
     [Parameter] public List<int>? AlarmDays { get; set; }
     [Parameter][EditorRequired] public bool AlarmActive { get; set; }
     [Parameter] public bool IsSnoozeVisible { get; set; }
@@ -30,6 +31,8 @@ public partial class Alarm
     public bool IsInSnooze { get; set; } = false;
 
     private int snoozing = 0;
+
+    private int today = -1;
 
     protected override Task OnInitializedAsync()
     {
@@ -74,6 +77,13 @@ public partial class Alarm
                     StateHasChanged();
                 }
                 break;
+            case AlarmStatus.STOPPED_TODAY:
+                if(today != e.Day || (e.Hour == AlarmHour && e.Minute != AlarmMinute))
+                {
+                    today = e.Day;
+                    Status= AlarmStatus.STOPPED;
+                }
+                break;
             default:
                 break;
         }
@@ -105,10 +115,12 @@ public partial class Alarm
         await JsRuntime.InvokeVoidAsync("StopSound");
     }
 
-    private void StopAlarm()
+    private async void StopAlarm()
     {
-        Status = AlarmStatus.STOPPED;
+        Status = AlarmStatus.STOPPED_TODAY;
+        today = DateTime.Today.Day;
         IsSnoozeVisible = false;
         snoozing = 0;
+        await JsRuntime.InvokeVoidAsync("StopSound");
     }
 }
