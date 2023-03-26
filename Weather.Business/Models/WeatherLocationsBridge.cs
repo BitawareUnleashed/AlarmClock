9b10ca945ae0ae3c00cab178ca8c85da6b33a502
BitawareUnleashed <dario.benevento@gmail.com>
2023-03-26 23:09:13 +0200

    Cities database in SQL file

--- Weather.Business/Models/WeatherLocationsBridge.cs
+++ Weather.Business/Models/WeatherLocationsBridge.cs
@@ -14,6 +14,32 @@ public class WeatherLocationsBridge
     public WeatherLocationsBridge(WeatherLocationsContext weatherLocationsContext)
     {
         this.weatherLocationsContext = weatherLocationsContext;
+
+
+
+        List<UiLocation> WeatherLocations = new List<UiLocation>();
+        try
+        {
+            // Call database to retrieve the location list
+            WeatherLocations = new List<UiLocation>();
+            var c = weatherLocationsContext.Cities.Where(x => x.Name.Contains("Liv"));
+            foreach (var location in c)
+            {
+                var newLocation = location.Adapt<UiLocation>();
+                //newLocation.Lat = location.Coord.Lat;
+                //newLocation.Long = location.Coord.Long;
+
+                newLocation.Lat = weatherLocationsContext.Coordinates.Where(x => x.coord_id == location.coord_id).FirstOrDefault().Lat;
+                newLocation.Long = weatherLocationsContext.Coordinates.Where(x => x.coord_id == location.coord_id).FirstOrDefault().Long;
+
+
+                WeatherLocations.Add(newLocation);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 
     public List<UiLocation> GetLocationList(string seatchLocation)
@@ -36,8 +62,6 @@ public class WeatherLocationsBridge
 
                 WeatherLocations.Add(newLocation);
             }
-
-
         }
         catch (Exception ex)
         {
