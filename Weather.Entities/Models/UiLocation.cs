87f27469ca3be34fae14ed124d5ed9eca6e7dd4d
BitawareUnleashed <dario.benevento@gmail.com>
2023-03-26 22:30:31 +0200

    Added search location (Draft)

---
+++ Weather.Entities/Models/UiLocation.cs
@@ -0,0 +1,10 @@
+﻿namespace Weather.Business.Models;
+
+public class UiLocation
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public double Lat { get; set; }
+    public double Long { get; set; }
+}
